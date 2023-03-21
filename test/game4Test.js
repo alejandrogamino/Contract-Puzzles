453da765066dc1954e49491bcb7378af004fee6f
alejandrogamino <alejandrogamino@pm.me>
2023-03-22 00:28:39 +0100

    Assingment 5.1 Contract Puzzles - solved

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,35 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
-
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+    return { game, signer1, signer2 };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer1, signer2 } = await loadFixture(deployContractAndSetVariables);
+
+    // contract Game4 {
+    //   bool public isWon;
+    
+    //   mapping(address => mapping(address => bool)) nested;
+    
+    //   function write(address x) external {
+    //     nested[x][msg.sender] = true;
+    //   }
+    
+    //   function win(address y) external {
+    //     require(nested[msg.sender][y], "Nope. Try again!");
+    
+    //     isWon = true;
+    //   }
+    // }
 
     // nested mappings are rough :}
+    // await game.connect(signer1).buy({ value: '2' });
 
-    await game.win();
+    await game.connect(signer1).write(signer2.getAddress());
+
+    await game.connect(signer2).win(signer1.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,70 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+
+    // We need a signer that has an address lesser or equal than the threshold
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+
+    // Get a signer
+    let signer = ethers.provider.getSigner(0);
+    // Get another signer to send funds later
+    let signer1 = ethers.provider.getSigner(1);
+
+    // Get the address of the signer
+    let address = (await signer.getAddress()) .toString();
+    // console.log(address);
+
+    // Safeguard var to avoid great loops
+    let attempts = 0;
+
+    // Loop until we get an address lesser or equal than the threshold
+    while (address > threshold) {
+      // Get another random signer
+      signer = ethers.Wallet.createRandom();
+
+      // Get the address of the new signer
+      address = (await signer.getAddress()).toString();
+
+      // update the number of attempts and exit the loop if we reach a certain number of attempts
+      attempts++;
+      if (attempts >= 1000) {
+        console.log("Cagada");
+        continue;
+      }
+    }
+    // console.log(address);
+    // Signer found
+    // Connect the signer to the provider
+    signer =  signer.connect(ethers.provider);
+    // Send 1 ETH from one of the initial signers to the random signer found
+    await signer1.sendTransaction({to: signer.address, value: ethers.utils.parseEther("1")});
+
+    // console.log("0x000fFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf" < "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf");
+    // while (signer.getAddress() > threshold) {
+      
+    // }
+
+    return { game, signer };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    // contract Game5 {
+    //   bool public isWon;
+    
+    //   address threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    
+    //   function win() external {
+    //     require(bytes20(msg.sender) < bytes20(threshold), "Nope. Try again!");
+    
+    //     isWon = true;
+    //   }
+    // }
 
-    await game.win();
+
+
+    await game.connect(signer).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
